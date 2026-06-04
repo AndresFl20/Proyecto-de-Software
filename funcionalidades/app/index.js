d66b15f9b5786b23899dfa7286c9e7babf966e21
Andres Felipe Luengas <andresluengas201211@gmail.com>
2026-06-04 01:23:34 -0500

    Update index.js
    Se ajusta para prueba de desplieguie.

--- funcionalidades/app/index.js
+++ funcionalidades/app/index.js
@@ -9,6 +9,17 @@ import courseController from "./controllers/course.controller.js";
 
 dotenv.config();
 
+console.log("URI:", process.env.MONGO_URI);
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("✅ MongoDB conectado");
+})
+.catch(err => {
+    console.error("❌ Error Mongo:");
+    console.error(err);
+});
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 
