8e2420bf2860210dcc4053751ac740051f286eb2
Andres Felipe Luengas <andresluengas201211@gmail.com>
2026-06-04 01:29:35 -0500

    Actualziación despliegue
    Se actualzan archivos prueba despliegue.

--- funcionalidades/app/index.js
+++ funcionalidades/app/index.js
@@ -11,15 +11,6 @@ dotenv.config();
 
 console.log("URI:", process.env.MONGO_URI);
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    console.log("✅ MongoDB conectado");
-})
-.catch(err => {
-    console.error("❌ Error Mongo:");
-    console.error(err);
-});
-
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 
